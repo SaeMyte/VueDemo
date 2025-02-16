19b1b4ce73fcfa1878c2c507c259f2b5c170a887
SaeMyte <tonia.zhu@outlook.com>
2025-02-16 22:02:03 +0800

    [feat]编写一个Person组件

---
+++ src/components/Person.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+export default {
+  name: "Person",
+  data() {
+    return {
+      name: "张三 ",
+      age: 18,
+      tel: "1388888888"
+    }
+  },
+  methods: {
+    showTel() {
+      alert(this.tel)
+    },
+    changeName() {
+      this.name = "李四"
+    },
+    changeAge() {
+      this.age += 1;
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="person">
+    <h2>姓名:{{ name }}</h2>
+    <h2>年龄:{{ age }}</h2>
+    <button @click="changeName">修改姓名</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<style scoped>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+button {
+  margin: 0 10px;
+}
+</style>
